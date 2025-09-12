0a9af2a6cf5c912fd6eda84fabfae5a0f8526568
Tobias Lenander <tobias.lenander@outlook.com>
2025-09-12 14:56:03 +0200

    Create module-info.java

--- src/main/java/com/dropalltables/App.java
+++ src/main/java/com/dropalltables/App.java
@@ -2,6 +2,7 @@ package com.dropalltables;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,11 +13,23 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        // Set a title to the window
+        primaryStage.setTitle("Drop All Tables");
+
+        // Create a root node
         VBox root = new VBox();
+        
+        // Add a Hello World label
+        Label helloLabel = new Label("Hello World!");
+        root.getChildren().add(helloLabel);
+        
+        // Create a new scene
         Scene scene = new Scene(root, 800, 600);
         
+        // Show the scene in the primary stage
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Drop All Tables");
         primaryStage.show();
+
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module db.dropalltables {
+    exports com.dropalltables;
+
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires transitive javafx.graphics;
+}
